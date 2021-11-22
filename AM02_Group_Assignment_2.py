e6d2ad6c1a0ce06d9daaa2be1dbdcc350a0a1fa3
Kazmer Nagy-Betegh <knagybetegh@yahoo.com>
2021-11-22 00:13:34 +0000

    first version of homework

--- AM02_Group_Assignment_2.py
+++ AM02_Group_Assignment_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Author : YOUR GROUP NUMBER GOES HERE
+Author : Group 11
 AM02 Group Assignment 2 
 """
 
@@ -22,6 +22,7 @@ pip install -i https://pypi.anaconda.org/berber/simple tweet-preprocessor
 pip install wordcloud
 '''
 
+import string
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt   # main plotting library
@@ -43,9 +44,15 @@ tweets = list(df_all['text']) # will allow us to use both pandas and numpy fns
 
 # Number of downloaded tweets (approx 3,500)
 
+print("Number of tweets: ", len(tweets))
+
 # First tweet
 
+print("First tweet: ", tweets[0])
+
 # Remove duplicate tweets from the list (re-tweets; news repeats)
+
+tweets = set(tweets)
 
 # --- CLEAN DATA
 # Based on which libraries will be used afterwards, some of the below data 
@@ -65,25 +72,36 @@ print(s)   # remember original string will be unchaned as it is immutable
 
 # Clean all tweets using list comprehension
 
+tweets = [p.clean(t) for t in tweets]
+
 # Check how the first 5 tweets look like now:
 
+print(tweets[:5])
 
 # Clean tweets using regular expressions. 
 # Keep only letters and replace everything else with a space. Syntax is:
 # re.sub(what you keep, what you repalce it with, string)
+tws1= list(df_all['text'])
 tws2 = [re.sub("[^a-zA-Z]", " ", x) for x in tws1] 
 
 # Let's check how the first 5 tweets look like now:
 
+print(tws2[:5])
 
 # Strip all tweets of start/end white spaces and place all text to lower case 
 # Use string methods to:
 # strip characters of all start/end white spaces
 # place all text to lower case 
 
+tweets = [x.strip().lower() for x in tweets]
+
 # Let's check how the first 5 tweets look like now:
 
+print(tweets[:5])
+
 # Delete any empty tweets (perhaps they only contained emojies)
+
+tweets = [x for x in tweets if len(x) > 0]
 
 
 #%% Q1 II. PERFORM SENTIMENT ANALYSIS -----------------------------------------------:
@@ -113,8 +131,15 @@ print(score) # 'compound': 0.0
 
 # b) TASK: WRITE CODE WHICH WOULD PRODUCE AV. 'compound' SCORE FOR TESLA TWEETS
 
+AV_compound_score = 0
 
+for tweet in tweets:
+    score = sid.polarity_scores(tweet)
+    AV_compound_score += score['compound']
 
+print("AV Compound score: ", AV_compound_score)
+
+print("Average compound score: ", AV_compound_score/len(tweets))
 
 # c) Comment on the final score
 
@@ -123,7 +148,9 @@ print(score) # 'compound': 0.0
 # Obtain words that should not be plotted as they are not of much interest
 nltk.download('stopwords') # download stopwords 
 sw = set(stopwords.words('english'))
-print(sw) # common Engish stopwords 
+print(sw) # common English stopwords 
+
+tws4 = [x for x in tweets if x not in sw]
 
 # Obtain all tweets as a single string to be used inside WordCloud function
 allTweets = " ".join(tws4) # join all tweets into single string 
@@ -154,22 +181,44 @@ fomc_sep2018 = ["the", "committee", "expects", "that", "further", "gradual", "in
 fomc_dec2018 = ["the", "committee", "judges", "that", "some", "further", "gradual", "increases", "in", "the", "target", "range"]
    
 
+# a)
+print(type(fomc_sep2018))
+print(type(fomc_dec2018))
+
+# b)
+# words seperated into strings
+
+#c)Check how many elements are in each container (i.e inside fomc_sep2018 and fomc_dec2018)
+print(len(fomc_sep2018))
+print(len(fomc_dec2018))
+
+# d) All words occurring in two sentences.
+# e) Words which were common for both sentences.
+# f) Words which were NOT used in the latest sentence. 
+# g) Words which were NEW in the latest sentence.
+
+from collections import defaultdict
+
+def sentece_compare(older, newer):
+
+    answer = {}
+    answer['all'] = older + newer
+    answer['common'] = [x for x in older if x in newer]
+    answer['new'] = [x for x in newer if x not in older]
+    answer['old'] = [x for x in older if x not in newer]
+
+    return answer
+
+print(sentece_compare(fomc_sep2018, fomc_dec2018))
+    
 
 
+'''
+{'all': ['the', 'committee', 'expects', 'that', 'further', 'gradual', 'increases', 'in', 'the', 'target', 'range', 'the', 'committee', 'judges', 'that', 'some', 'further', 'gradual', 'increases', 'in', 'the', 'target', 'range'], 
+'common': ['the', 'committee', 'that', 'further', 'gradual', 'increases', 'in', 'the', 'target', 'range'], 
+'new': ['judges', 'some'], 
+'old': ['expects']}
+'''
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# %%
